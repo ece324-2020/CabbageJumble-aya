179ef338bd08ec546f9b5e2434a6426bb61e13fa
thedavidchu <thedavidchu@gmail.com>
2020-11-10 19:26:25 -0500

    Added data and stuff

--- main.py
+++ main.py
@@ -100,7 +100,7 @@ def main():
     pygame.display.update()
     pos = None
     myfont = pygame.font.SysFont("fontname", 50)
-    count = 562
+    count = 410
     while True:
          clock.tick(90)
          draw_buttons(surface,8)
