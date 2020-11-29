2ee3f3d2420d7ff6de0478e47c985425aec200c7
James Han <jameshanjameshan@gmail.com>
2020-11-29 12:48:25 -0500

    Model for no heads or tials

--- Baseline/Classification/create_state_dict.py
+++ Baseline/Classification/create_state_dict.py
@@ -4,4 +4,4 @@ import torch
 
 model = torch.load("model_noHT.pt")
 
-new_model = torch.save(model.state_dict(), "model_state_dict3.pt")
+new_model = torch.save(model.state_dict(), "model_state_dict_noHT.pt")
